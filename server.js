b84f47a9f7355bd4ff3444e4b164ebf3af4e362d
Ankit <londeybakchod@gmail.com>
2026-07-02 18:29:56 +0530

    Add explicit route for index.html

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// Force the server to serve index.html for the home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
